374dc147f35ab8814ca04f3a7df7311205be2fe9
jaehoonlee <jaelee9212>
2023-03-10 12:39:53 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Link, Routes, Route } from 'react-router-dom'
+import { Page } from './pages/Home/Page'
+
+import{useEffect} from 'react';
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Page></Page>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
